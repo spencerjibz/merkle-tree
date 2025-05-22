d5d9ceab3d960dd6e493bfd57b43431e264f5748
Spencerjibz <spencernajib2@gmail.com>
2025-05-22 16:45:55 +0100

    just iterator methods to build tree

--- bin/main.rs
+++ bin/main.rs
@@ -1,14 +1,15 @@
 use merkle_tree::{example_data, hash_data, MerkleTree};
 fn main() {
-    let data = example_data(4);
+    let now = std::time::Instant::now();
+    let data = example_data(100000);
     let index = data.len() - 1;
     let tree = MerkleTree::construct(&data);
     let h1 = &data[index];
-    tree.pretty_print();
+    //tree.pretty_print();
     let selected = hex::encode(hash_data(h1));
     let header_bars = "-".repeat(selected.len() / 4);
     println!("{} Path to h1 {} {}", header_bars, &selected, header_bars);
-    tree.print_data_route(h1);
+    //tree.print_data_route(h1);
     println!("{}", "-".repeat(selected.len() + 20));
     println!("prove for {} at index {index}", &selected);
     let proof = tree.prove(h1);
@@ -16,7 +17,8 @@ fn main() {
     if let Some(proof) = proof {
         // verify here;
         let pretty_proof = proof.get_proof_in_hex();
-        println!("{pretty_proof:#?}");
+        //println!("{pretty_proof:#?}");
         assert!(MerkleTree::verify_proof(h1, &proof, root_hash));
     }
+    println!("{:?}", now.elapsed());
 }
