c01e0cc50832b2267c9940f10a8458ce951ab847
Spencerjibz <spencernajib2@gmail.com>
2025-06-24 00:00:12 +0100

    heap memory profiling

--- bin/main.rs
+++ bin/main.rs
@@ -1,26 +1,11 @@
 use merkle_tree::{hash_data, stores::create_large_input_byes_sled, MerkleTree};
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profile = dhat::Profiler::new_heap();
     let now = std::time::Instant::now();
-    /* let data = example_data(100000);
-        let index = data.len() - 1;
-        let tree = MerkleTree::construct(&data);
-        let h1 = &data[index];
-        //tree.pretty_print();
-        let selected = hex::encode(hash_data(h1));
-        let header_bars = "-".repeat(selected.len() / 4);
-        println!("{} Path to h1 {} {}", header_bars, &selected, header_bars);
-        //tree.print_data_route(h1);
-        println!("{}", "-".repeat(selected.len() + 20));
-        println!("prove for {} at index {index}", &selected);
-        let proof = tree.prove(h1);
-        let root_hash = tree.root();
-        if let Some(proof) = proof {
-            // verify here;
-            let pretty_proof = proof.get_proof_in_hex();
-            //println!("{pretty_proof:#?}");
-            assert!(MerkleTree::verify_proof(h1, &proof, root_hash));
-        }
-    */
     append_multiple_to_un_balanced_tree();
     println!("{:?}", now.elapsed());
 }
